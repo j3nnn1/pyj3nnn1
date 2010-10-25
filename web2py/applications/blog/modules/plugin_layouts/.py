348f3c9d69939caff769a065de04c693b1bff1a1
j3nnn1 <jcmm986@gmail.com>
2010-10-25 01:19:35 -0430

    estilos, y paginacion. entre cosas de maquiclub

---
+++ web2py/applications/blog/modules/plugin_layouts/.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python 
+# coding: utf8 
+from gluon.html import *
+from gluon.http import *
+from gluon.validators import *
+from gluon.sqlhtml import *
+# request, response, session, cache, T, db(s) 
+# must be passed and cannot be imported!
