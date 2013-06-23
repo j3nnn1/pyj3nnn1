8208742b16c560d02c79719c0c2213e9767ec2f7
j3nnn1 <jcmm986@gmail.com>
2013-06-23 00:19:54 -0300

    smart format to TREC default terrier format:W

--- xml/toxmlTREC.py
+++ xml/toxmlTREC.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# j3nnn1 - 0.0.0 -
 #.I identificador del documento
 #
 #DOC
 # DOCNO
-#
-from xml.dom import minidom
-#import xml.etree.ElementTree as ET
+# S
+# M
+# T
+# P
+# W
+# A
+#/DOC
+from xml.dom import minidom # for a pretty xml
 #import xml.etree.ElementTree as xml
-from lxml import etree as xml
+from lxml import etree as xml # it's better than only ET
 import os
-import re
+import re #testing re
 
 try:
     file = open('corpus_evaluar/ohsumed.87', 'r');
 except IOError:
     print('Hola que hace?, File not exist!')
 except:
-    print('Ups.. Error inesperado!')
-
-
-#x = ('xxx', 'abcxxxabc', 'xyx', 'abc', 'x.x', 'axa', 'axxxxa', 'axxya')
-
-#dir(filter((lambda s: re.match(r'xxx', s)), x))
+    print('Ups.. Unexpected Error!')
 
 pattern = re.compile(r'(\d+)')
-
 
 root = xml.Element('terrier')
 #this is bad, I know. :(  Python FILE on memory RAM. I don't know other way
@@ -35,58 +33,58 @@ size = len(lines)
 
 for count, line  in enumerate(lines):
     nextline = count + 1 
-    if (re.match('\.I', line)):
-        id = pattern.search(line).group(0)
+    #if (re.match('\.I', line)): # much procesador not good
+    if (line[0:2].strip()=='.I'):
+        #id = pattern.search(line).group(0) 
         child = xml.Element('DOC')      ##create child
         root.append(child)              ##add 
 
-    if(re.match('\.U', line)):
-    
+    if(line.strip()=='.U'):
         if (nextline < size):
             docno = lines[nextline].strip()
             child2 = xml.Element('DOCNO')   ##setting attr DOCNO
             child2.text = docno
             child.append(child2)
-    if (re.match('\.S',line)):
+    if (line.strip()=='.S'):
         if (nextline < size):
             source = lines[nextline].strip()
             child2 = xml.Element('S')   ##setting attr DOCNO
             child2.text = source
             child.append(child2)
-    if (re.match('\.M',line)):
+    if (line.strip()=='.M'):
         if (nextline < size):
             source = lines[nextline].strip()
             child2 = xml.Element('M')   ##setting attr DOCNO
             child2.text = source
             child.append(child2)
-    if (re.match('\.T',line)):
+    if (line.strip()=='.T'):
         if (nextline < size):
             source = lines[nextline].strip()
             child2 = xml.Element('T')   ##setting attr DOCNO
             child2.text = source
             child.append(child2)
  
-    if (re.match('\.P',line)):
+    if (line.strip()=='.P'):
         if (nextline < size):
             source = lines[nextline].strip()
             child2 = xml.Element('P')   ##setting attr DOCNO
             child2.text = source
             child.append(child2)
 
-    if (re.match('\.W',line)):
+    if (line.strip()=='.W'):
         if (nextline < size):
             source = lines[nextline].strip()
             child2 = xml.Element('W')   ##setting attr DOCNO
             child2.text = source
             child.append(child2)
 
-    if (re.match('\.A',line)):
+    if (line.strip()=='.A'):
         if (nextline < size):
             source = lines[nextline].strip()
             child2 = xml.Element('A')   ##setting attr DOCNO
             child2.text = source
             child.append(child2)
 
-xml.ElementTree(root).write('testing.xml', pretty_print=True)
+xml.ElementTree(root).write('ohsumed87.xml', pretty_print=True)
 file.close()
 
